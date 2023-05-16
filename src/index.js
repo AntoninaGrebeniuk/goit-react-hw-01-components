5c5fa8745c95710be7631228d6d97224ec79c44b
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-05-17 01:19:58 +0300

    Create user jsx

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// const profile = (
+//   <div class="profile">
+//     <div class="description">
+//       <img src={user.avatar} alt={user.username} class="avatar" />
+//       <p class="name">{user.username}</p>
+//       <p class="tag">@{user.tag}</p>
+//       <p class="location">{user.location}</p>
+//     </div>
+
+//     <ul class="stats">
+//       <li>
+//         <span class="label">Followers</span>
+//         <span class="quantity">{user.stats.followers}</span>
+//       </li>
+//       <li>
+//         <span class="label">Views</span>
+//         <span class="quantity">{user.stats.views}</span>
+//       </li>
+//       <li>
+//         <span class="label">Likes</span>
+//         <span class="quantity">{user.stats.likes}</span>
+//       </li>
+//     </ul>
+//   </div>
+// );
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
