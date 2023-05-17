1059ed89bec6df8d0ba6c181346617e99ac2b785
Antonina Grebeniuk <rebel.t2808@gmail.com>
2023-05-17 15:24:40 +0300

    Create statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
 import user from './data/user';
+import data from './data/data';
+import friends from './data/friends';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      avatar={user.avatar}
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
+    <section>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+    </section>
   );
 };
